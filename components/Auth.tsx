eb855b93fc6d27875a9cd38d227877c9672a2811
Harpo Barrett <hbar0009@student.monash.edu>
2022-04-09 13:10:09 +1000

    feat: add signup to auth component

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -3,6 +3,7 @@ import { supabase } from "../utils/supabaseClient";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Link from "next/link";
+import { Accordion } from "react-bootstrap";
 
 export default function Auth() {
   const [loading, setLoading] = useState(false);
@@ -30,47 +31,97 @@ export default function Auth() {
   };
 
   const handleSignUp = async (email: string, password: string) => {
-    await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      });
+      if (error) throw error;
+      alert("Signed in successfully!");
+    } catch (error: any) {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="auth-form">
-      <h3 id="LoginHeading">Log In</h3>
-      <hr />
-      <Form
-        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-          e.preventDefault();
-          handleLogin(email, password);
-        }}
-      >
-        <Form.Group controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button
-          style={{ marginTop: "30px" }}
-          type="submit"
-          disabled={!validateForm()}
-        >
-          <span>{loading ? "Loading..." : "Login"}</span>
-        </Button>
-      </Form>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Log In 🔐</Accordion.Header>
+          <Accordion.Body>
+            <Form
+              onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+                e.preventDefault();
+                handleLogin(email, password);
+              }}
+            >
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  autoFocus
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button
+                style={{ marginTop: "30px" }}
+                type="submit"
+                disabled={!validateForm()}
+              >
+                <span>{loading ? "Loading..." : "Login"}</span>
+              </Button>
+            </Form>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Sign Up 📋</Accordion.Header>
+          <Accordion.Body>
+            <Form
+              onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+                e.preventDefault();
+                handleSignUp(email, password);
+              }}
+            >
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  autoFocus
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button
+                style={{ marginTop: "30px" }}
+                type="submit"
+                disabled={!validateForm()}
+              >
+                <span>{loading ? "Loading..." : "Sign Up"}</span>
+              </Button>
+            </Form>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 }
