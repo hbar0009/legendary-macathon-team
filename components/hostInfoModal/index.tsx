d9943ac76da92f1f8bf67f2594b3b2962fc4be2a
jfan0010 <48777884+jfan0010@users.noreply.github.com>
2022-04-10 10:04:27 +1000

    dynamically change event host modal

---
+++ components/hostInfoModal/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Button, Modal, ModalProps } from "react-bootstrap";
+import { Omit, BsPrefixProps } from "react-bootstrap/esm/helpers";
+
+const HostInfoModal = (
+  props: JSX.IntrinsicAttributes &
+    Omit<
+      Pick<
+        React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLDivElement>,
+          HTMLDivElement
+        >,
+        "key" | keyof React.HTMLAttributes<HTMLDivElement>
+      > & {
+        ref?:
+          | ((instance: HTMLDivElement | null) => void)
+          | React.RefObject<HTMLDivElement>
+          | null
+          | undefined;
+      },
+      BsPrefixProps<"div"> & ModalProps
+    > &
+    BsPrefixProps<"div"> &
+    ModalProps & { children?: React.ReactNode }
+) => {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton />
+      <Modal.Body>
+        <h4>{props.hostInfo.name}</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default HostInfoModal;
